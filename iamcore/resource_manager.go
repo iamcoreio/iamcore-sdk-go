228f23f7da3f94906c0b6f07e0b8830d61f74f55
Vitalii Kolomiiets <vkolomiiets@kaaiot.io>
2023-06-24 10:25:08 +0000

    IAM-144: updates due to new iamcore api

--- iamcore/resource_manager.go
+++ iamcore/resource_manager.go
@@ -2,30 +2,32 @@ package iamcore
 
 import (
 	"context"
+	"net/http"
 
 	"gitlab.kaaiot.net/core/lib/iamcore/irn.git"
 )
 
 type ResourceManager interface {
-	// CreateResource creates resource on iamcore using the API key.
+	// CreateResource creates resource on iamcore.
 	//
 	// Returns ErrSDKDisabled error in case SDK is disabled.
 	// Returns ErrUnauthenticated error in case of unauthenticated access.
 	// Returns ErrConflict error in case duplicated resource found.
 	// Returns ErrForbidden error in case authenticated principal does not have sufficient permissions to create the resource.
 	// Returns ErrUnknown error in case of unexpected response from iamcore server.
-	CreateResource(ctx context.Context, tenantID, resourceType, resourcePath, resourceID string) error
+	CreateResource(ctx context.Context, authorizationHeader http.Header, application, tenantID, resourceType, resourcePath, resourceID string) error
 
-	// DeleteResource deletes resource on iamcore using the API key.
+	// DeleteResource deletes resource on iamcore.
 	//
 	// Returns ErrSDKDisabled error in case SDK is disabled.
 	// Returns ErrUnauthenticated error in case of unauthenticated access.
 	// Returns ErrForbidden error in case authenticated principal does not have sufficient permissions to delete the resource.
 	// Returns ErrUnknown error in case of unexpected response from iamcore server.
-	DeleteResource(ctx context.Context, tenantID, resourceType, resourcePath, resourceID string) error
+	DeleteResource(ctx context.Context, authorizationHeader http.Header, application, tenantID, resourceType, resourcePath, resourceID string) error
 }
 
-func (c *сlient) CreateResource(ctx context.Context, tenantID, resourceType, resourcePath, resourceID string) error {
+func (c *сlient) CreateResource(ctx context.Context, authorizationHeader http.Header, application, tenantID, resourceType, resourcePath, resourceID string,
+) error {
 	if c.disabled {
 		return ErrSDKDisabled
 	}
@@ -36,29 +38,31 @@ func (c *сlient) CreateResource(ctx context.Context, tenantID, resourceType, re
 
 	createResourceRequestDTO := CreateResourceRequestDTO{
 		Name:         resourceID,
+		Application:  application,
 		ResourceType: resourceType,
 		Path:         resourcePath,
 		Enabled:      true,
 		TenantID:     tenantID,
 	}
 
-	return c.iamcoreClient.CreateResource(ctx, c.GetAPIKeyAuthorizationHeader(), createResourceRequestDTO)
+	return c.iamcoreClient.CreateResource(ctx, authorizationHeader, createResourceRequestDTO)
 }
 
-func (c *сlient) DeleteResource(ctx context.Context, tenantID, resourceType, resourcePath, resourceID string) error {
+func (c *сlient) DeleteResource(ctx context.Context, authorizationHeader http.Header, application, tenantID, resourceType, resourcePath, resourceID string,
+) error {
 	if c.disabled {
 		return ErrSDKDisabled
 	}
 
-	principalIRN, err := c.iamcoreClient.GetPrincipalIRN(ctx, c.GetAPIKeyAuthorizationHeader())
+	principalIRN, err := c.iamcoreClient.GetPrincipalIRN(ctx, authorizationHeader)
 	if err != nil {
 		return err
 	}
 
-	resourceIRN, err := irn.NewIRN(principalIRN.GetAccountID(), "iamcore", tenantID, nil, resourceType, irn.SplitPath(resourcePath), resourceID)
+	resourceIRN, err := irn.NewIRN(principalIRN.GetAccountID(), application, tenantID, nil, resourceType, irn.SplitPath(resourcePath), resourceID)
 	if err != nil {
 		return err
 	}
 
-	return c.iamcoreClient.DeleteResource(ctx, c.GetAPIKeyAuthorizationHeader(), resourceIRN)
+	return c.iamcoreClient.DeleteResource(ctx, authorizationHeader, resourceIRN)
 }
